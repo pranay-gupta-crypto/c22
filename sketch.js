a3de28de6e5ecdb8f0bd0d55fc0e8eda2d37aa11
pranay-gupta-crypto <78412312+pranay-gupta-crypto@users.noreply.github.com>
2021-06-06 13:50:29 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -11,10 +11,10 @@ const Body = Matter.Body;
 
 function preload()
 {
-	starImg = loadImage("images/star.png");
-	bgImg = loadImage("images/starNight.png");
-	fairyImg = loadAnimation("images/fairyImage1.png","images/fairyImage2.png");
-	fairySound = loadSound("sound/JoyMusic.mp3");
+	starImg = loadImage("star.png");
+	bgImg = loadImage("starNight.png");
+	fairyImg = loadAnimation("fairyImage1.png","fairyImage2.png");
+	fairySound = loadSound("JoyMusic.mp3");
 	//load animation for fairy here
 }
 
